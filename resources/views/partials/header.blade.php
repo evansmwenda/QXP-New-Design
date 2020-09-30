c5e02e4e26444633937811302249b15a6485a485
evansmwenda <evansmwenda.em@gmail.com>
2020-09-30 13:07:33 +0300

    blading templates now done

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<meta charset="utf-8">
+
+<!-- Mobile Specific Metas ================================-->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Site Title- -->
+<title>SeoBin - SEO, Social Media and Marketing HTML Template</title>
+
+<!-- CSS
+==================================================== -->
+<!--Font Awesome -->
+<link rel="stylesheet" href="css/font-awesome.min.css" />
+
+<!-- Animate CSS -->
+<link rel="stylesheet" href="css/animate.css">
+
+<!-- Iconic Fonts -->
+<link rel="stylesheet" href="css/icofonts.css" />
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<!-- Owl Carousel -->
+<link rel="stylesheet" href="css/owlcarousel.min.css" />
+
+<!-- Video Popup -->
+<link rel="stylesheet" href="css/magnific-popup.css" />
+
+<!--Style CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<!--Responsive CSS -->
+<link rel="stylesheet" href="css/responsive.css">
+
+
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
