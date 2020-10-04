94a243947f7501dad025f4d7e37ecd135c2c6473
evansmwenda <evansmwenda.em@gmail.com>
2020-10-04 22:33:38 +0300

    style changes to features section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -678,7 +678,7 @@
          </div>
      
          <div class="col-lg-6 ml-auto col-md-12 wow fadeInRight" data-wow-duration="1s">
-         <img src="{{asset('images/featured/Asset1.png')}}" width="600">
+         <img src="{{asset('images/featured/Asset1.png')}}" width="100%">
          </div>
          <!-- End Col -->
       </div>
@@ -691,7 +691,7 @@
 <section id="tw-facts" class="tw-facts">
    <div class="container"> 
       <div class="row">
-         <div class="facts">
+         <div class="facts" style="margin:0px 15px;">
          <h3>QXP Features</h3>
          <p>We Have You Covered for All Your Workplace, Collaboration and Training Needs.</p>
          </div>
@@ -699,69 +699,151 @@
       <br>  
       <!-- Row End -->
       <div class="row">
-         <div class="col-md-4"> 
-               <div class="facts-img">
-                  <img src="{{asset('images/icons/005-video-call-2.svg')}}" width="50" height="60">           
-                  <span>Video & Audio Conferencing</span>
-               </div>
-               <br>
-               <div class="facts-img">
-                  <img src="{{asset('images/icons/001-mobile.svg')}}" width="50" height="60">           
-                  <span>Easy-to-Use Mobile App</span>
-               </div>
-               <br>
-               <div class="facts-img">
-                  <img src="{{asset('images/icons/002-videoconference.svg')}}" width="50" height="60">           
-                  <span>Breakout Rooms</span>
-               </div>
-               <br>
-               <div class="facts-img">
-                  <img src="{{asset('images/icons/003-video-call-1.svg')}}" width="50" height="60">           
-                  <span>Comprehensive Recording</span>
-               </div>
-         </div>
-         <div class="col-md-4"> 
+         <div class="col-md-4 col-sm-12 features-section" style="margin: 20px 0px; ">
             <div class="facts-img">
-               <img src="{{asset('images/icons/012-online-learning.svg')}}" width="50" height="60">           
-               <span>Learning Management System</span>
-            </div>
-            <br>
-            <div class="facts-img">
-               <img src="{{asset('images/icons/003-sharing.svg')}}" width="50" height="60">           
-               <span>Screen Sharing</span>
-            </div>
-            <br>
-            <div class="facts-img">
-               <img src="{{asset('images/icons/007-poll.svg')}}" width="50" height="60">           
-               <span>Polling & Audience Evaluation</span>
-            </div>
-            <br>
-            <div class="facts-img">
-               <img src="{{asset('images/icons/011-report.svg')}}" width="50" height="60">           
-               <span>Detailed Analytics & Reporting</span>
+               <div class="row">
+                  <div class="col-md-2 col-sm-12" style="padding:auto;margin-right: 0px !important">
+                     <img src="{{asset('images/icons/005-video-call-2.svg')}}" width="50" height="60">
+                  </div>
+                  <div class="col-md-10 col-sm-12" style="padding: 15px 15px !important;margin-left: 0px !important"><span style="margin:0px !important;padding: 0px !important;">Video & Audio Conferencing</span></div>
+               </div>
+                          
+               <!-- <span>Easy-to-Use Mobile App</span> -->
             </div>
          </div>
-         <div class="col-md-4"> 
+
+         <div class="col-md-4 col-sm-12 features-section" style="margin: 20px 0px; ">
             <div class="facts-img">
-               <img src="{{asset('images/icons/010-user-interface.svg')}}" width="50" height="60">           
-               <span>Simple, Intuitive User Interface</span>
+               <div class="row">
+                  <div class="col-md-2 col-sm-12" style="padding:auto;margin-right: 0px !important">
+                     <img src="{{asset('images/icons/001-mobile.svg')}}" width="50" height="60">
+                  </div>
+                  <div class="col-md-10 col-sm-12" style="padding: 15px 15px !important;margin-left: 0px !important"><span style="margin:0px !important;padding: 0px !important;">Easy-to-Use Mobile App</span></div>
+               </div>
             </div>
-            <br>
+         </div>
+
+         <div class="col-md-4 col-sm-12 features-section" style="margin: 20px 0px; ">
             <div class="facts-img">
-               <img src="{{asset('images/icons/009-record.svg')}}" width="50" height="60">           
-               <span>Live Video Streaming</span>
+               <div class="row">
+                  <div class="col-md-2 col-sm-12" style="padding:auto;margin: auto">
+                     <img src="{{asset('images/icons/002-videoconference.svg')}}" width="50" height="60">
+                  </div>
+                  <div class="col-md-10 col-sm-12" style="padding: 15px 15px !important;margin-left: 0px !important"><span style="margin:0px !important;padding: 0px !important;">Breakout Rooms</span></div>
+               </div>
+                          
+               <!-- <span>Easy-to-Use Mobile App</span> -->
             </div>
-            <br>
+         </div>
+
+         <div class="col-md-4 col-sm-12 features-section" style="margin: 20px 0px; ">
             <div class="facts-img">
-               <img src="{{asset('images/icons/022-reliability.svg')}}" width="50" height="60">           
-               <span>Support & Security</span>
+               <div class="row">
+                  <div class="col-md-2 col-sm-12" style="padding:auto;margin: auto">
+                     <img src="{{asset('images/icons/003-video-call-1.svg')}}" width="50" height="60">
+                  </div>
+                  <div class="col-md-10 col-sm-12" style="padding: 15px 15px !important;margin-left: 0px !important"><span style="margin:0px !important;padding: 0px !important;">Comprehensive Recording</span></div>
+               </div>
+                          
+               <!-- <span>Easy-to-Use Mobile App</span> -->
             </div>
-            <br>
+         </div>
+
+         <div class="col-md-4 col-sm-12 features-section" style="margin: 20px 0px; ">
             <div class="facts-img">
-               <img src="{{asset('images/icons/013-board.svg')}}" width="50" height="60">           
-               <span>Multi User White Board</span>
+               <div class="row">
+                  <div class="col-md-2 col-sm-12" style="padding:auto;margin: auto">
+                     <img src="{{asset('images/icons/012-online-learning.svg')}}" width="50" height="60">
+                  </div>
+                  <div class="col-md-10 col-sm-12" style="padding: 15px 15px !important;margin-left: 0px !important"><span style="margin:0px !important;padding: 0px !important;">Learning Management System</span></div>
+               </div>
+                          
+               <!-- <span>Easy-to-Use Mobile App</span> -->
             </div>
-      </div>
+         </div>
+
+         <div class="col-md-4 col-sm-12 features-section" style="margin: 20px 0px; ">
+            <div class="facts-img">
+               <div class="row">
+                  <div class="col-md-2 col-sm-12" style="padding:auto;margin: auto">
+                     <img src="{{asset('images/icons/003-sharing.svg')}}" width="50" height="60">
+                  </div>
+                  <div class="col-md-10 col-sm-12" style="padding: 15px 15px !important;margin-left: 0px !important"><span style="margin:0px !important;padding: 0px !important;">Screen Sharing</span></div>
+               </div>
+                          
+               <!-- <span>Easy-to-Use Mobile App</span> -->
+            </div>
+         </div>
+
+
+         <div class="col-md-4 col-sm-12 features-section" style="margin: 20px 0px; ">
+            <div class="facts-img">
+               <div class="row">
+                  <div class="col-md-2 col-sm-12" style="padding:auto;margin: auto">
+                     <img src="{{asset('images/icons/007-poll.svg')}}" width="50" height="60">
+                  </div>
+                  <div class="col-md-10 col-sm-12" style="padding: 15px 15px !important;margin-left: 0px !important"><span style="margin:0px !important;padding: 0px !important;">Polling & Audience Evaluation</span></div>
+               </div>
+                          
+               <!-- <span>Easy-to-Use Mobile App</span> -->
+            </div>
+         </div>
+
+         <div class="col-md-4 col-sm-12 features-section" style="margin: 20px 0px; ">
+            <div class="facts-img">
+               <div class="row">
+                  <div class="col-md-2 col-sm-12" style="padding:auto;margin: auto">
+                     <img src="{{asset('images/icons/011-report.svg')}}" width="50" height="60">
+                  </div>
+                  <div class="col-md-10 col-sm-12" style="padding: 15px 15px !important;margin-left: 0px !important"><span style="margin:0px !important;padding: 0px !important;">Detailed Analytics & Reporting</span></div>
+               </div>
+            </div>
+         </div>
+
+         <div class="col-md-4 col-sm-12 features-section" style="margin: 20px 0px; ">
+            <div class="facts-img">
+               <div class="row">
+                  <div class="col-md-2 col-sm-12" style="padding:auto;margin: auto">
+                     <img src="{{asset('images/icons/010-user-interface.svg')}}" width="50" height="60">
+                  </div>
+                  <div class="col-md-10 col-sm-12" style="padding: 15px 15px !important;margin-left: 0px !important"><span style="margin:0px !important;padding: 0px !important;">Simple, Intuitive User Interface</span></div>
+               </div>
+            </div>
+         </div>
+
+         <div class="col-md-4 col-sm-12 features-section" style="margin: 20px 0px; ">
+            <div class="facts-img">
+               <div class="row">
+                  <div class="col-md-2 col-sm-12" style="padding:auto;margin: auto">
+                     <img src="{{asset('images/icons/009-record.svg')}}" width="50" height="60">
+                  </div>
+                  <div class="col-md-10 col-sm-12" style="padding: 15px 15px !important;margin-left: 0px !important"><span style="margin:0px !important;padding: 0px !important;">Live Video Streaming</span></div>
+               </div>
+            </div>
+         </div>
+
+         <div class="col-md-4 col-sm-12 features-section" style="margin: 20px 0px; ">
+            <div class="facts-img">
+               <div class="row">
+                  <div class="col-md-2 col-sm-12" style="padding:auto;margin: auto">
+                     <img src="{{asset('images/icons/022-reliability.svg')}}" width="50" height="60">
+                  </div>
+                  <div class="col-md-10 col-sm-12" style="padding: 15px 15px !important;margin-left: 0px !important"><span style="margin:0px !important;padding: 0px !important;">Support & Security</span></div>
+               </div>
+            </div>
+         </div>
+
+         <div class="col-md-4 col-sm-12 features-section" style="margin: 20px 0px; ">
+            <div class="facts-img">
+               <div class="row">
+                  <div class="col-md-2 col-sm-12" style="padding:auto;margin: auto">
+                     <img src="{{asset('images/icons/013-board.svg')}}" width="50" height="60">
+                  </div>
+                  <div class="col-md-10 col-sm-12" style="padding: 15px 15px !important;
+                  margin-left: 0px !important"><span style="margin:0px !important;padding: 0px !important;">Multi User White Board</span></div>
+               </div>
+            </div>
+         </div>
 
       </div>
    </div>
