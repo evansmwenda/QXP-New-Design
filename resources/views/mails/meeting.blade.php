af206f9e6723f54a0c0757a5b98bb69a0f2fec57
geff-mutua <geoffreymutua01@gmail.com>
2020-11-13 16:55:55 +0300

    New Email COnfiguration settings?

--- resources/views/mails/meeting.blade.php
+++ resources/views/mails/meeting.blade.php
@@ -5,18 +5,27 @@
     <title>QXP Meeting</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
-<body style="background: #E5E8E8">
-    <div style="background:#E5E8E8 ; text-align:center; height:300px"> 
+<body>
+<!--     <div style="background:#E5E8E8 ; text-align:center; height:300px"> 
     <img src="http://sandbox.qxp-global.com/images/logo/logo.svg" width="100" alt="" style="margin-top: 80px;">
-    </div>
-    <div class="content" style="background: #fff; width:80%; margin:-100px auto; padding:50px">
-        <h2 style="color:#060646">Join QXP Meeting</h2>
-        <h4>{{$data['name']}} has invited you to a scheduled QXP Meeting</h4>
-    <p>Meeting Link is : <a href="{{$data['link']}}">{{$data['link']}}</a></p>
-        <p>Meeting ID : {{$data['ID']}}</p>
+    </div> -->
+   
+        <h2 style=>QXP Meeting Creation was successfull</h2>
+        <p>Dear {{$data['name']}},</p>
+         <p>You been has invited you to a scheduled QXP Meeting</h4>
+
+         <p>Meeting ID : {{$data['ID']}}</p>
 
         <p>Topic : {{$data['topic']}}</p>
+
+        <br>
+    <a href="{{$data['link']}}"><button style="background: #11BECC; border-radius: 20px; border: 1px solid transparent; padding: 8px; width: 300px; color: #fff" 
+            >CLICK HERE TO JOIN</button></a>
+            <br><br>
+            <p>Best Regards,</p>
+            <p>QXP Support Team</p>
+            <br><br>
         <i style="text-align: center; margin-top:30px">This mail is send automatically please dont reply to it.</i>
-    </div>
+  
     </body>
 </html>
