786c08498d9c11a37f307a21867661e07002600a
geff-mutua <geoffreymutua01@gmail.com>
2020-11-14 10:41:44 +0300

    new changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -36,7 +36,7 @@
                             </div>
     
                             <div class="form-group row">
-                                <label for="email" class="col-form-label text-md-right">{{ __('E-Mail Address') }}</label>S
+                                <label for="email" class="col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
     
                                     @error('email')
@@ -55,10 +55,7 @@
                             </div>
     
                             <div class="form-group row">
-                                <label for="password" class=" col-form-label text-md-right">{{ __('Password') }}</label>
-    
-                              
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                    <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
     
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
@@ -69,8 +66,7 @@
                             </div>
     
                             <div class="form-group row">
-                                <label for="password-confirm" class=" col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                               <input id="password-confirm" placeholder="Confirm-Password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                                 </div>
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
