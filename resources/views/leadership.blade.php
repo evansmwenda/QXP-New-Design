717e69a0753ad9b62fc5f46d568bef1999ec4d2a
geff-mutua <geoffreymutua01@gmail.com>
2020-11-20 14:59:14 +0300

    New leadership update

--- resources/views/leadership.blade.php
+++ resources/views/leadership.blade.php
@@ -14,20 +14,20 @@
         <img src="{{asset('images/profiles/kevin.jpg')}}" alt="">
             <h3>Kevin Baraza</h3>
             <h4>Founder</h4>   
-            <span class="fa fa-linkedin"></span>     
+            <a href="https://www.linkedin.com/in/kevin-baraza-6a7a1b16/" target="new"><span class="fa fa-linkedin"></span> </a>   
     </div>
             <div class="profiles">
         <img src="{{asset('images/profiles/mathew.png')}}" alt="">
             <h3>Mathew Mwangi</h3>
             <h4>CO-Founder</h4>  
-            <span class="fa fa-linkedin"></span>
+            <a href="https://www.linkedin.com/in/matthew-mwangi-2ba29419/" target="new"><span class="fa fa-linkedin"></span> </a>
 
           </div>
           <div class="profiles">
             <img src="{{asset('images/profiles/mary.jpg')}}" alt="">
                 <h3>Mary Kinyua</h3>
                 <h4>Co-Founder</h4>  
-                <span class="fa fa-linkedin"></span>
+                <a href="https://www.linkedin.com/in/mary-kinyua/" target="new"><span class="fa fa-linkedin"></span> </a>
         </div>
 
 </div>
@@ -36,20 +36,20 @@
         <img src="{{asset('images/profiles/george.jpg')}}" alt="">
             <h3>George Kahinga</h3>
             <h4>Co-Founder</h4>  
-            <span class="fa fa-linkedin"></span>
+            <a href="https://www.linkedin.com/in/george-kahinga-muiruri-2bb51344/" target="new"><span class="fa fa-linkedin"></span> </a>
     </div>
     <div class="profiles">
         <img src="{{asset('images/profiles/kirimi.jpg')}}" alt="">
             <h3>Kirimi Mpungu</h3>
             <h4>Co-Founder</h4>
-            <span class="fa fa-linkedin"></span>
+            <a href="https://www.linkedin.com/in/kirimi-mpungu-7823239/" target="new"><span class="fa fa-linkedin"></span> </a>
 
            </div>
     <div class="profiles">
         <img src="{{asset('images/profiles/grace.jpg')}}" alt="">
             <h3>Grace Muinde</h3>
             <h4>Co-Founder</h4>  
-            <span class="fa fa-linkedin"></span>
+            <a href="https://www.linkedin.com/in/cpa-grace-muinde-a06987163/" target="new"><span class="fa fa-linkedin"></span> </a>
 
      </div>
 </div>
@@ -58,14 +58,14 @@
         <img src="{{asset('images/profiles/doreen.jpeg')}}" alt="">
             <h3>Doreen Gakii</h3>
             <h4>CO-Founder</h4> 
-            <span class="fa fa-linkedin"></span> 
+            <a href="https://www.linkedin.com/in/doreen-gakii-970b2068/" target="new"><span class="fa fa-linkedin"></span> </a> 
 
     </div>
     <div class="profiles">
         <img src="{{asset('images/profiles/#.jpg')}}" alt="">
             <h3>Sam Njau</h3>
             <h4>Co-Founder</h4> 
-            <span class="fa fa-linkedin"></span>
+            <a href="https://www.linkedin.com/in/sam-njau-9898b189/" target="new"><span class="fa fa-linkedin"></span> </a> 
 
      </div>
 </div>
@@ -89,12 +89,11 @@
                      <div class="col-md-7">
                         <h3>Kevin Baraza</h3>
                         <h4>Founder</h4> 
-                        <p>A small river named Duden flows by their place and supplies it with the necessary</p>
-                        <p><strong>Phone:</strong> +1 (859) 254-6589</p>
-                        <p><strong>Email:</strong> info@example.com</p>
-                        <span class="fa fa-linkedin"></span> 
+                        <p>A Seasoned business expert acknowledged throughout his career for capacity to cultivate strategic relationships and build trust, while simultaneously steering positive bottom line results. Kevin has worked in many different roles as a Financial Modelling Expert, Strategy Analyst and now sits as a Managing Director</p>
+                        <a href="https://www.linkedin.com/in/kevin-baraza-6a7a1b16/  " target="new"><span class="fa fa-linkedin"></span> </a>
+
                      </div>
-                 </div>
+                      </div>
             
     
             </div>
